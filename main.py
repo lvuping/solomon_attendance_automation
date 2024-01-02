af30112cd78e68947815a1f8465374abddcc4675
lvuping <lvupingg@gmail.com>
2024-01-02 02:00:24 +0100

    조상욱 위원님 API키 추가

--- main.py
+++ main.py
@@ -8,9 +8,13 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-USER_ID = os.getenv('USER_ID')
-API_KEY = os.getenv('API_KEY')
-TIMEZONE_NAME = os.getenv('TIMEZONE_NAME')
+USER_ID1 = os.getenv("USER_ID1")
+API_KEY1 = os.getenv("API_KEY1")
+
+USER_ID2 = os.getenv("USER_ID2")
+API_KEY2 = os.getenv("API_KEY2")
+
+TIMEZONE_NAME = os.getenv("TIMEZONE_NAME")
 
 START_HOUR_IN_MORNING = 7
 END_HOUR = 17
@@ -20,32 +24,36 @@ END_HOUR = 17
 
 def get_start_of_work(target_date):
     random_minute = random.randint(45, 57)
-    target_date = target_date.replace(hour=START_HOUR_IN_MORNING, minute=random_minute, second=0, microsecond=0)
-    return target_date.strftime('%Y-%m-%dT%H:%M:%S.000Z')
+    target_date = target_date.replace(
+        hour=START_HOUR_IN_MORNING, minute=random_minute, second=0, microsecond=0
+    )
+    return target_date.strftime("%Y-%m-%dT%H:%M:%S.000Z")
 
 
 def get_end_of_work(target_date):
     random_minute = random.randint(1, 59)
-    target_date = target_date.replace(hour=END_HOUR, minute=random_minute, second=0, microsecond=0)
-    return target_date.strftime('%Y-%m-%dT%H:%M:%S.000Z')
+    target_date = target_date.replace(
+        hour=END_HOUR, minute=random_minute, second=0, microsecond=0
+    )
+    return target_date.strftime("%Y-%m-%dT%H:%M:%S.000Z")
 
 
-def do_attendance(target_date):
+def do_attendance(target_date, api_key, user_id):
     payload = {
-        'userId': USER_ID,
-        'start': get_start_of_work(target_date),
-        'end': get_end_of_work(target_date),
-        'timezoneName': TIMEZONE_NAME,
-        # 'end': None, #you set None, You need to finish manually
-        # 'timezone': TIMEZONE,
-        'type': 'work'
+        "userId": user_id,
+        "start": get_start_of_work(target_date),
+        "end": get_end_of_work(target_date),
+        "timezoneName": TIMEZONE_NAME,
+        "type": "work",
     }
 
-    url = 'https://app.absence.io/api/v2/timespans/create'
+    url = "https://app.absence.io/api/v2/timespans/create"
     data = json.dumps(payload)
-    hawk_auth = HawkAuth(id=USER_ID, key=API_KEY, server_url=url)
+    hawk_auth = HawkAuth(id=user_id, key=api_key, server_url=url)
 
-    request_response = requests.post(url, auth=hawk_auth, data=data, headers={'Content-Type': 'application/json'})
+    request_response = requests.post(
+        url, auth=hawk_auth, data=data, headers={"Content-Type": "application/json"}
+    )
 
     return request_response
 
@@ -63,15 +71,24 @@ def main():
             continue  # Skip processing today
 
         if target_date.weekday() < 5:  # Check if it's a weekday
-            response = do_attendance(target_date)
-            if response is not False and response.ok:
-                print(f'Done: {target_date}')
+            response1 = do_attendance(target_date, API_KEY1, USER_ID1)
+            response2 = do_attendance(target_date, API_KEY2, USER_ID2)
+            if (
+                response1 is not False
+                and response1.ok
+                and response2 is not False
+                and response2.ok
+            ):
+                print(f"Done: {target_date}")
             else:
-                print('Error: ' + response.text)
+                print("Error: " + response1.text)
+                print("Error: " + response2.text)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--days_ago', type=int, help='Number of days ago to start from', default=30)
+    parser.add_argument(
+        "--days_ago", type=int, help="Number of days ago to start from", default=30
+    )
     args = parser.parse_args()
     main()
