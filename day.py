f68ba1cafaba415207d6b8eb2ac9c32473d359df
lvuping <lvupingg@gmail.com>
2025-01-17 21:34:45 +0900

    day version

--- day.py
+++ day.py
@@ -35,14 +35,19 @@ def get_end_of_work(target_date):
     return target_date.strftime("%Y-%m-%dT%H:%M:%S.000Z")
 
 
-def do_attendance(target_date, api_key, user_id):
+def do_attendance(target_date, api_key, user_id, is_end=False):
+    # 기본 payload 구성
     payload = {
         "userId": user_id,
-        "start": get_start_of_work(target_date),
-        "end": get_end_of_work(target_date),
         "timezoneName": TIMEZONE_NAME,
         "type": "work",
     }
+
+    # 출퇴근 여부에 따라 시간 필드 추가
+    if is_end:
+        payload["end"] = get_end_of_work(target_date)
+    else:
+        payload["start"] = get_start_of_work(target_date)
 
     url = "https://app.absence.io/api/v2/timespans/create"
     data = json.dumps(payload)
@@ -57,28 +62,26 @@ def do_attendance(target_date, api_key, user_id):
 
 def main():
     current_date = datetime.utcnow()
-    days_ago = args.days_ago  # Get the number of days ago from the argument
 
-    start_date = current_date - timedelta(days=days_ago)
+    if current_date.weekday() < 5:  # 평일인 경우에만 실행
+        current_hour = current_date.hour
 
-    for i in range(days_ago + 1):
-        target_date = start_date + timedelta(days=i)
-
-        # if target_date.date() == current_date.date():  # Check if target_date is today
-        #     continue  # Skip processing today
-
-        if target_date.weekday() < 5:  # Check if it's a weekday
-            response1 = do_attendance(target_date, API_KEY1, USER_ID1)
+        # 오전 출근 기록
+        if current_hour < END_HOUR:
+            response1 = do_attendance(current_date, API_KEY1, USER_ID1, is_end=False)
             if response1.ok:
-                print(f"Done: {target_date}")
+                print(f"출근 기록 완료: {current_date}")
             else:
-                print("Error on " + str(target_date) + ": " + response1.text)
+                print("출근 기록 오류: " + str(current_date) + ": " + response1.text)
+
+        # 오후 퇴근 기록
+        if current_hour >= END_HOUR:
+            response1 = do_attendance(current_date, API_KEY1, USER_ID1, is_end=True)
+            if response1.ok:
+                print(f"퇴근 기록 완료: {current_date}")
+            else:
+                print("퇴근 기록 오류: " + str(current_date) + ": " + response1.text)
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--days_ago", type=int, help="Number of days ago to start from", default=5
-    )
-    args = parser.parse_args()
     main()
